51b6fa0f4498dbd306fe6dba920f7e2a6ed615dc
BlueGlassBlock <blueglassblock@outlook.com>
2021-08-16 23:04:03 +0800

    hotfix: automatically create missing directories

--- lib/path.py
+++ lib/path.py
@@ -1,7 +1,20 @@
-from os.path import abspath, join
+from os import makedirs
+from os.path import abspath, join, exists
 
 root = abspath(join(__file__, "..", ".."))
 config = abspath(join(root, "config"))
 log = abspath(join(root, "log"))
 plugin = abspath(join(root, "plugin"))
 database = abspath(join(root, "database"))
+
+if not exists(config):
+    makedirs(config)
+
+if not exists(log):
+    makedirs(log)
+
+if not exists(plugin):
+    makedirs(plugin)
+
+if not exists(database):
+    makedirs(database)
