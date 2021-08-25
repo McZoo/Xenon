37c1ef5ce18dcaa02a39336fc204383b82b57459
StarGazer4K <57087368+StarGazer4K@users.noreply.github.com>
2021-08-25 15:09:56 +0800

    毒鸡汤插件
    从http://api.btstu.cn/yan/api.php获取的毒鸡汤

---
+++ plugin/soup.py
@@ -0,0 +1,38 @@
+# coding=utf-8
+"""
+毒鸡汤，StarGazer4k投稿的第一个插件
+"""
+import secrets
+
+import aiohttp
+from graia.application import MessageChain, GraiaMiraiApplication
+from graia.application.message.elements.internal import Plain
+from graia.saya import Saya, Channel
+from graia.saya.builtins.broadcast import ListenerSchema
+
+
+from lib.command import CommandEvent
+
+LIMIT = 100
+
+__version__ = "1.0.0"
+__author__ = "StarGazer4K"
+__plugin_name__ = "soup"
+__plugin_doc__ = f"""\
+.soup 获取一碗毒鸡汤
+"""
+
+saya = Saya.current()
+channel = Channel.current()
+
+
+@channel.use(ListenerSchema(listening_events=[CommandEvent]))
+async def dice(app: GraiaMiraiApplication, event: CommandEvent):
+    if event.user and event.command in ".soup":
+        async with aiohttp.request(
+                "GET", "http://api.btstu.cn/yan/api.php"
+        ) as response:
+            json_str = await response.text()
+        await event.send_result(
+            MessageChain.create([Plain(json_str)])
+        )
