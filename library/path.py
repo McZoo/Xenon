c76876c7a5fbac013b51df2b664a132a75ec8994
BlueGlassBlock <blueglassblock@outlook.com>
2022-02-10 21:10:38 +0800

    :heavy_plus_sign: cave module

--- library/path.py
+++ library/path.py
@@ -9,3 +9,7 @@ asset.mkdir(parents=True, exist_ok=True)
 config: Path = Path(root, "config")
 
 config.mkdir(parents=True, exist_ok=True)
+
+database: Path = Path(root, "database")
+
+database.mkdir(parents=True, exist_ok=True)
--- module/__init__.py
+++ module/__init__.py
@@ -1,7 +1,8 @@
 import os
+import pathlib
 
-__all__ = [
-    s.removesuffix(".py")
-    for s in os.listdir(os.path.dirname(__file__))
-    if s.endswith(".py") and not s.startswith("_")
-]
+__all__ = []
+
+for path in pathlib.Path(os.path.dirname(__file__)).iterdir():
+    if (path.is_dir() or path.name.endswith(".py")) and not path.name.startswith("_"):
+        __all__.append(path.stem)
